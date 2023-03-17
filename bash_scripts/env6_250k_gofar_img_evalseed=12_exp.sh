59165033e097eba6f7c37235f92a524a639162bb
Chongyi Zheng <chongyiz@chongyis-mbp.wifi.local.cmu.edu>
2023-03-17 11:33:38 -0400

    exp with single task

--- bash_scripts/env6_250k_gofar_img_evalseed=12_exp.sh
+++ bash_scripts/env6_250k_gofar_img_evalseed=12_exp.sh
@@ -15,7 +15,7 @@ export PYTHONPATH=$PYTHONPATH:$HOME/offline_c_learning/railrl-private
 export PATH=$PATH:$HOME/anaconda3/envs/railrl/bin
 export LOG_ROOT="/projects/rsalakhugroup/chongyiz/offline_c_learning/GoFAR/saved_models/SawyerEnv6/SawyerEnv6-0.1-0.0-gofar-20disc0.01-relabel0.0-0"
 
-declare -a seeds=(0 1)
+declare -a seeds=(0)
 
 for seed in "${seeds[@]}"; do
   export CUDA_VISIBLE_DEVICES=$seed
